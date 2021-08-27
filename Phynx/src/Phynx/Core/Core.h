d1b0ae0cd37139cb6adf9890d3563303913b44be
Akhil Jose <akhiljose@gmail.com>
2021-08-27 07:32:43 +0530

    put in PX namespace

--- Phynx/src/Phynx/Core/Core.h
+++ Phynx/src/Phynx/Core/Core.h
@@ -1,18 +1,22 @@
 #pragma once
 #include <memory>
 
-// Graphics API enumeration
-enum class API
-{
-	OpenGL, // in progress
-	D3D12,  // not implemented
-	D3D11,  // not implemented
-	Vulkan, // not implemented
-	None,
-};
+namespace PX {
 
-template<class T>
-using Shared = std::shared_ptr<T>;
+	// Graphics API enumeration
+	enum class API
+	{
+		OpenGL, // in progress
+		D3D12,  // not implemented
+		D3D11,  // not implemented
+		Vulkan, // not implemented
+		None,
+	};
 
-template<class T>
-using Unique = std::unique_ptr<T>;
+	template<class T>
+	using Shared = std::shared_ptr<T>;
+
+	template<class T>
+	using Unique = std::unique_ptr<T>;
+
+}
