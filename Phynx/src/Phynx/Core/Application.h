d4670e32ffcd6b9c8f8816a9ad2a48e8aedb6718
Akhil Jose <akhiljose@gmail.com>
2021-08-27 07:36:01 +0530

    include and use renderer
    add description for CreateApplicationWindow

--- Phynx/src/Phynx/Core/Application.h
+++ Phynx/src/Phynx/Core/Application.h
@@ -3,6 +3,7 @@
 #include "../Renderer/VertexArray.h"
 #include "../Renderer/Buffer.h"
 #include "../Renderer/Shader.h"
+#include "../Renderer/Renderer.h"
 
 namespace PX {
 
@@ -16,10 +17,21 @@ namespace PX {
 
 		static Application* GetApplicationInstance();
 		static Shared<Window> GetApplicationWindow();
+
+
+		/// <summary>
+		/// Creates a window.
+		/// Despite the name, it does more than create the window.
+		/// it sets the API to use, prepares other functions such as
+		/// logging, the renderer api implementation.
+		/// API specific changes cannot be done after calling this function
+		/// </summary>
+		/// <param name="data">The window creation data</param>
 		static void CreateApplicationWindow(WindowCreateStruct& data);
 
 	protected:
 		virtual void Update();
+		virtual void Render();
 
 	private:
 		static inline Application* m_pInstance = nullptr;
