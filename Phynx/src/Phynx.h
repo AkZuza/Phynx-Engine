472edf7428691ad777d9045a589f7f4a3f14db36
Akhil Jose <akhiljose@gmail.com>
2021-08-27 07:33:00 +0530

    Add necessary headers

--- Phynx/src/Phynx.h
+++ Phynx/src/Phynx.h
@@ -1,6 +1,10 @@
 
 #include "Phynx/Core/Core.h"
 #include "Phynx/Core/Application.h"
+#include "Phynx/Renderer/VertexArray.h"
+#include "Phynx/Renderer/Buffer.h"
+#include "Phynx/Renderer/Shader.h"
+#include "Phynx/Renderer/Renderer.h"
 
 
 // Entry Point
