a64b7c1a39323520d8a47fd2a54776173c835634
Akhil Jose <akhiljose@gmail.com>
2021-08-31 06:29:29 +0530

    call Renderer2D::Init()

--- Phynx/src/Phynx/Renderer/Renderer.cpp
+++ Phynx/src/Phynx/Renderer/Renderer.cpp
@@ -2,11 +2,14 @@
 #include "Renderer.h"
 #include "API/OpenGL/OpenGLRenderer.h"
 
+#include "Renderer2D.h"
+
 namespace PX {
 
 	void Renderer::Init()
 	{
 		m_RendererAPI = Shared<Renderer>(new OpenGLRenderer());
+		Renderer2D::Init();
 	}
 
 	void Renderer::Draw(uint32_t count, Shared<VertexBuffer> buffer, Shared<Shader> shader)
