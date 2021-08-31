8a26f30a585a996bbdefbc67237574fd0562a9ed
Akhil Jose <akhiljose@gmail.com>
2021-08-31 06:49:46 +0530

    log the context information

--- Phynx/src/Phynx/API/OpenGL/OpenGLContext.cpp
+++ Phynx/src/Phynx/API/OpenGL/OpenGLContext.cpp
@@ -1,5 +1,6 @@
 #include "pxpch.h"
 #include "OpenGLContext.h"
+#include "Logging/Log.h"
 
 #include <glad/glad.h>
 #define GLFW_INCLUDE_NONE
@@ -11,6 +12,19 @@ namespace PX {
 	{
 		glfwMakeContextCurrent((GLFWwindow*)handle);
 		gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+
+		// output context description
+		std::string vendor = (char*)glGetString(GL_VENDOR);
+		std::string renderer = (char*)glGetString(GL_RENDERER);
+		std::string version = (char*)glGetString(GL_VERSION);
+		std::string shading_version = (char*)glGetString(GL_SHADING_LANGUAGE_VERSION);
+
+		PX_CORE_INFO("API: OpenGL");
+		PX_CORE_INFO("Vendor: {0}", vendor);
+		PX_CORE_INFO("Renderer: {0}", renderer);
+		PX_CORE_INFO("Version: {0}", version);
+		PX_CORE_INFO("Shading Language Version: {0}", shading_version);
 	}
 
 	OpenGLContext::~OpenGLContext()
