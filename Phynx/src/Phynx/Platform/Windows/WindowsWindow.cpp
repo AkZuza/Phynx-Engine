6eabc57e3ad2dec061563143d948d2ec3ee29f2b
Akhil Jose <akhiljose@gmail.com>
2021-09-30 06:59:59 +0530

    add callbacks to handle and push events

--- Phynx/src/Phynx/Platform/Windows/WindowsWindow.cpp
+++ Phynx/src/Phynx/Platform/Windows/WindowsWindow.cpp
@@ -1,6 +1,7 @@
 #include "pxpch.h"
 #include "WindowsWindow.h"
 #include "Logging/Log.h"
+#include "Event/Event.h"
 
 #define GLFW_INCLUDE_NONE 1
 #include <GLFW/glfw3.h>
@@ -41,6 +42,41 @@ namespace PX {
 		m_WindowData.Flags = data.Flags;
 
 		glfwSetWindowUserPointer((GLFWwindow*)m_pWindow, &m_WindowData);
+
+
+		// Set event callbacks
+		glfwSetWindowPosCallback((GLFWwindow*)m_pWindow, [](GLFWwindow* win, int x, int y)
+			{
+				//PX_CORE_INFO("Position changed X:{0}  Y:{1}", x, y);
+				WindowMoveEvent event(x, y);
+				EventDispatcher::SendEvent(event);
+			});
+
+		glfwSetErrorCallback([](int code, const char* msg)
+			{
+				PX_CORE_ERROR("Error Code {0} : {1}", code, msg);
+			});
+
+		glfwSetWindowSizeCallback((GLFWwindow*)m_pWindow, [](GLFWwindow* window, int width, int height)
+			{
+				//PX_CORE_INFO("Window Size X:{0}  Y:{1}", width, height);
+				WindowResizeEvent event(width, height);
+				EventDispatcher::SendEvent(event);
+
+			});
+
+		glfwSetWindowCloseCallback((GLFWwindow*)m_pWindow, [](GLFWwindow* window)
+			{
+				//PX_CORE_INFO("Window Closed");
+				WindowCloseEvent event;
+				EventDispatcher::SendEvent(event);
+			});
+
+		glfwSetCursorPosCallback((GLFWwindow*)m_pWindow, [](GLFWwindow* window, double x, double y)
+			{
+				MouseMoveEvent event(x, y);
+				EventDispatcher::SendEvent(event);
+			});
 	}
 
 	bool WindowsWindow::IsRunning()
