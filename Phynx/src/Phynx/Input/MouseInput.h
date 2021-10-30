9da53ddcc3fc45a0b92e972340e92d4b1b2cd16e
Akhil Jose <akhiljose@gmail.com>
2021-10-30 08:32:01 +0530

    Revert "implement mouse input"
    This reverts commit fe42e9e4185a6b5e69fd6f631aec78faaeafb538.

--- Phynx/src/Phynx/Input/MouseInput.h
+++ Phynx/src/Phynx/Input/MouseInput.h
@@ -3,19 +3,11 @@
 
 namespace PX {
 
-	enum class MouseButton
-	{
-		Left = 27, Right, Middle
-	};
-
 	class MouseInput
 	{
 	public:
 		static double GetXPos();
 		static double GetYPos();
-
-		static bool IsPressed(MouseButton keyCode);
-		static bool IsReleased(MouseButton keyCode);
 	};
 
 
