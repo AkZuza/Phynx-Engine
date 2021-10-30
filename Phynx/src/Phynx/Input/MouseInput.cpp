fe42e9e4185a6b5e69fd6f631aec78faaeafb538
Akhil Jose <akhiljose@gmail.com>
2021-10-30 08:30:33 +0530

    implement mouse input

--- Phynx/src/Phynx/Input/MouseInput.cpp
+++ Phynx/src/Phynx/Input/MouseInput.cpp
@@ -22,4 +22,37 @@ namespace PX {
 		return y;
 	}
 
+	bool MouseInput::IsPressed(MouseButton keyCode)
+	{
+		uint32_t glfwCode = 0;
+
+		switch (keyCode)
+		{
+		case MouseButton::Left: glfwCode = GLFW_MOUSE_BUTTON_LEFT; break;
+		case MouseButton::Right: glfwCode = GLFW_MOUSE_BUTTON_RIGHT; break;
+		case MouseButton::Middle: glfwCode = GLFW_MOUSE_BUTTON_MIDDLE; break;
+
+		default:
+			return false;
+		}
+
+		return glfwGetMouseButton((GLFWwindow*)Application::GetApplicationWindow()->GetWindowHandle(), glfwCode) == GLFW_PRESS;
+	}
+
+	bool MouseInput::IsReleased(MouseButton keyCode)
+	{
+		uint32_t glfwCode = 0;
+
+		switch (keyCode)
+		{
+		case MouseButton::Left: glfwCode = GLFW_MOUSE_BUTTON_LEFT; break;
+		case MouseButton::Right: glfwCode = GLFW_MOUSE_BUTTON_RIGHT; break;
+		case MouseButton::Middle: glfwCode = GLFW_MOUSE_BUTTON_MIDDLE; break;
+
+		default:
+			return false;
+		}
+
+		return glfwGetMouseButton((GLFWwindow*)Application::GetApplicationWindow()->GetWindowHandle(), glfwCode) == GLFW_RELEASE;
+	}
 }
--- Phynx/src/Phynx/Input/MouseInput.h
+++ Phynx/src/Phynx/Input/MouseInput.h
@@ -3,11 +3,19 @@
 
 namespace PX {
 
+	enum class MouseButton
+	{
+		Left = 27, Right, Middle
+	};
+
 	class MouseInput
 	{
 	public:
 		static double GetXPos();
 		static double GetYPos();
+
+		static bool IsPressed(MouseButton keyCode);
+		static bool IsReleased(MouseButton keyCode);
 	};
 
 
